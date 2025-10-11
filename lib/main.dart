e13dc661570bf09aaadc000ab856ef3f23055444
samyak <samyakmhrzn9841@gmail.com>
2025-10-11 17:31:15 +0545

    inital App Layouts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:habitican/pages/add_screen.dart';
+import 'package:habitican/pages/dashboard.dart';
+import 'package:habitican/pages/edit_screen.dart';
+import 'package:habitican/pages/profile_screen.dart';
+import 'package:habitican/pages/tracker_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +14,88 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: 'habitician',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(useMaterial3: true),
+      home: AppLayout(),
+    );
+  }
+}
+
+class AppLayout extends StatefulWidget {
+  const AppLayout({super.key});
+
+  @override
+  State<AppLayout> createState() => _AppLayoutState();
+}
+
+class _AppLayoutState extends State<AppLayout> {
+  int currentPage = 0;
+
+  List<Widget> pages = [
+    Dashboard(),
+    EditScreen(),
+    AddScreen(),
+    TrackerScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: currentPage,
+        children: pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedFontSize: 0,
+        unselectedFontSize: 0,
+        currentIndex: currentPage,
+        onTap: (value) {
+          setState(() {
+            currentPage = value;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: Colors.white,
+            ),
+            label: '',
+          ),
+          // BottomNavigationBarItem(
+          //   icon: Icon(
+          //     Icons.edit_calendar_outlined,
+          //     color: Colors.white,
+          //   ),
+          //   label: '',
+          // ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.add_box_outlined,
+              color: Colors.white,
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.track_changes,
+              color: Colors.white,
+            ),
+            label: '',
+          ),
+          // BottomNavigationBarItem(
+          //   icon: Icon(
+          //     Icons.account_circle,
+          //     color: Colors.white,
+          //   ),
+          //   label: '',
+          // ),
+        ],
+      ),
+    );
   }
 }
