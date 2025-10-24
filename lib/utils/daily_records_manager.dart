368e32c664442324044e98eb3175e6fa05499544
samyak <samyakmhrzn9841@gmail.com>
2025-10-24 13:26:31 +0545

    Edge cases for daily record tracking

--- lib/utils/daily_records_manager.dart
+++ lib/utils/daily_records_manager.dart
@@ -17,24 +17,74 @@ Future<void> dailyRecordsManager() async {
 
   // print(habitsList.values.toList()[0].id.runtimeType);
 
-  final checkExisting = dailyRecordsBox.get(
-    todayDate,
-  );
+  final checkExisting = dailyRecordsBox.get(todayDate);
   print(checkExisting);
+
+  if (checkExisting != null) {
+    return;
+  }
+  if (dailyRecordsBox.keys.isNotEmpty) {
+    final lastDailyRecord = dailyRecordsBox.keys.last;
+    print(lastDailyRecord);
+    Duration difference = parsedDate.difference(
+      DateTime.parse(lastDailyRecord),
+    );
+    print(difference.inDays);
+    if (difference.inDays != 0) {
+      dailyRecordsBox.put(
+        todayDate,
+        DailyRecord(
+          date: todayDate,
+          habitId: habitsList.isNotEmpty
+              ? habitsList.map((e) => e.id).toList() as List<int>?
+              : [],
+          habitName: habitsList.isNotEmpty
+              ? habitsList.map((e) => e.name).toList() as List<String>?
+              : [],
+          isHabitCompleted: List.filled(habitsList.length, false),
+          taskId: tasksList.isNotEmpty
+              ? tasksList.map((e) => e.id).toList() as List<int>?
+              : [],
+          taskName: tasksList.isNotEmpty
+              ? tasksList.map((e) => e.name).toList() as List<String>?
+              : [],
+          isTaskCompleted: List.filled(tasksList.length, false),
+          dailyTaskId: dailyTasksList.isNotEmpty
+              ? dailyTasksList.map((e) => e.id).toList() as List<int>?
+              : [],
+          dailyTaskName: dailyTasksList.isNotEmpty
+              ? dailyTasksList.map((e) => e.name).toList() as List<String>?
+              : [],
+          isDailyTaskCompleted: List.filled(dailyTasksList.length, false),
+        ),
+      );
+    }
+  }
   if (checkExisting == null) {
     dailyRecordsBox.put(
       todayDate,
       DailyRecord(
         date: todayDate,
-        habitId: habitsList.map((e) => e.id).toList() as List<int>?,
-        habitName: habitsList.map((e) => e.name).toList() as List<String>?,
+        habitId: habitsList.isNotEmpty
+            ? habitsList.map((e) => e.id).toList() as List<int>?
+            : [],
+        habitName: habitsList.isNotEmpty
+            ? habitsList.map((e) => e.name).toList() as List<String>?
+            : [],
         isHabitCompleted: List.filled(habitsList.length, false),
-        taskId: tasksList.map((e) => e.id).toList() as List<int>?,
-        taskName: tasksList.map((e) => e.name).toList() as List<String>?,
+        taskId: tasksList.isNotEmpty
+            ? tasksList.map((e) => e.id).toList() as List<int>?
+            : [],
+        taskName: tasksList.isNotEmpty
+            ? tasksList.map((e) => e.name).toList() as List<String>?
+            : [],
         isTaskCompleted: List.filled(tasksList.length, false),
-        dailyTaskId: dailyTasksList.map((e) => e.id).toList() as List<int>?,
-        dailyTaskName:
-            dailyTasksList.map((e) => e.name).toList() as List<String>?,
+        dailyTaskId: dailyTasksList.isNotEmpty
+            ? dailyTasksList.map((e) => e.id).toList() as List<int>?
+            : [],
+        dailyTaskName: dailyTasksList.isNotEmpty
+            ? dailyTasksList.map((e) => e.name).toList() as List<String>?
+            : [],
         isDailyTaskCompleted: List.filled(dailyTasksList.length, false),
       ),
     );
