2756d8c5fe626d7bc533c1c2533faafe977e8b50
samyak <samyakmhrzn9841@gmail.com>
2025-10-25 16:04:10 +0545

    counter for habits and tasks

--- lib/pages/tracker_screen.dart
+++ lib/pages/tracker_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:habitican/database/boxes.dart';
+import 'package:habitican/database/dailyRecord.dart';
 import 'package:habitican/utils/calendarr.dart';
 import 'package:habitican/utils/day_progress_circular_bar.dart';
 import 'package:habitican/utils/global_state_provider.dart';
@@ -18,6 +20,64 @@ class _TrackerScreenState extends State<TrackerScreen> {
   Widget build(BuildContext context) {
     final globalState = Provider.of<GlobalStateProvider>(context);
     final selectedDate = globalState.selectedDate;
+
+    DailyRecord? selectedDateRecord = boxDailyRecords.get(
+      selectedDate.toString().split(" ")[0],
+    );
+
+    if (selectedDateRecord == null) {
+      return Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Your Progress Report"),
+            ],
+          ),
+          const SizedBox(
+            height: 500,
+            child: MonthlyScreen(),
+          ),
+          // DayProgressCircularBar(),
+          Expanded(
+            child: ListView(
+              children: [
+                Text('Daily Insights'),
+                Text(selectedDate.toString()),
+                Text("No Record for the given date present :( "),
+                Text("View Details ->"),
+              ],
+            ),
+          ),
+        ],
+      );
+    }
+
+    int completedHabits =
+        selectedDateRecord.isHabitCompleted?.where((e) => e == true).length ??
+        0;
+    int completedTasks =
+        selectedDateRecord.isTaskCompleted?.where((e) => e == true).length ?? 0;
+    int completedDailyTasks =
+        selectedDateRecord.isDailyTaskCompleted
+            ?.where((e) => e == true)
+            .length ??
+        0;
+
+    int habitLength = selectedDateRecord.isHabitCompleted!.length;
+    int taskLength = selectedDateRecord.isTaskCompleted!.length;
+    int dailyTasksLength = selectedDateRecord.isDailyTaskCompleted!.length;
+
+    double habitPercent = selectedDateRecord.isHabitCompleted!.isNotEmpty
+        ? completedHabits / habitLength
+        : 0;
+    double taskPercent = selectedDateRecord.isTaskCompleted!.isNotEmpty
+        ? completedHabits / taskLength
+        : 0;
+    double dailyTaskPercent =
+        selectedDateRecord.isDailyTaskCompleted!.isNotEmpty
+        ? completedHabits / dailyTasksLength
+        : 0;
 
     return Column(
       children: [
@@ -45,11 +105,17 @@ class _TrackerScreenState extends State<TrackerScreen> {
                 children: [
                   Column(
                     children: [
-                      Text("Habits Completed: 7/9"),
-                      Text("Task Completed: 9/11"),
+                      Text("Habits Completed: $completedHabits / $habitLength"),
+                      Text(
+                        "Task Completed: ${completedTasks + completedDailyTasks} / ${taskLength + dailyTasksLength}",
+                      ),
                     ],
                   ),
-                  DayProgressCircularBar(date: '1'),
+                  DayProgressCircularBar(
+                    date: '1',
+                    habitPercent: habitPercent,
+                    taskPercent: (taskPercent + dailyTaskPercent) / 2,
+                  ),
                 ],
               ),
               Text("View Details ->"),
--- lib/utils/day_progress_circular_bar.dart
+++ lib/utils/day_progress_circular_bar.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:percent_indicator/flutter_percent_indicator.dart';
 
 class DayProgressCircularBar extends StatelessWidget {
-  final double percent;
+  final double habitPercent;
+  final double taskPercent;
   final String date;
   const DayProgressCircularBar({
     super.key,
-    this.percent = 0.65,
+    this.habitPercent = 0.85,
+    this.taskPercent = 0.65,
     required this.date,
   });
 
@@ -15,7 +17,7 @@ class DayProgressCircularBar extends StatelessWidget {
     return CircularPercentIndicator(
       radius: 22.0,
       lineWidth: 5.0,
-      percent: percent + 0.2,
+      percent: habitPercent,
       circularStrokeCap: CircularStrokeCap.round,
       backgroundColor: Colors.transparent,
       progressColor: Colors.green,
@@ -23,7 +25,7 @@ class DayProgressCircularBar extends StatelessWidget {
       center: CircularPercentIndicator(
         radius: 15.0,
         lineWidth: 5.0,
-        percent: percent,
+        percent: taskPercent,
         circularStrokeCap: CircularStrokeCap.round,
         backgroundColor: Colors.transparent,
         progressColor: Colors.red,
